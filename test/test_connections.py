231546bac1f85eca2bb6ee299a6739e16f50d21d
Matt Bennett <matt@bennett.name>
2013-04-17 11:19:55 +0100

    Fixed tests Updated requirements

--- test/test_connections.py
+++ test/test_connections.py
@@ -5,20 +5,20 @@ from orp.connection import get_connection
 
 def test_temp_connection_defaults():
     conn = get_connection('temp://')
-    assert conn == "http://localhost:7475/db/data/"
+    assert conn.__uri__ == "http://localhost:7475/db/data/"
 
 
 def test_temp_connection_custom_port():
     port = "7777"
     conn = get_connection('temp://{}'.format(port))
-    assert conn == "http://localhost:{}/db/data/".format(port)
+    assert conn.__uri__ == "http://localhost:{}/db/data/".format(port)
 
 
 def test_temp_connection_custom_data_dir():
     data_dir = '/tmp/foo'
 
     conn = get_connection('temp://{}'.format(data_dir))
-    assert conn == "http://localhost:7475/db/data/"
+    assert conn.__uri__ == "http://localhost:7475/db/data/"
     assert os.path.exists(data_dir)
 
 
@@ -27,5 +27,5 @@ def test_temp_connection_custom():
     data_dir = '/tmp/foo'
 
     conn = get_connection('temp://{}{}'.format(port, data_dir))
-    assert conn == "http://localhost:{}/db/data/".format(port)
+    assert conn.__uri__ == "http://localhost:{}/db/data/".format(port)
     assert os.path.exists(data_dir)
