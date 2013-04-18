a5175f725e0c8126cbdca2a50c87aaeacdd7dac7
Tony Bajan <tony.bajan@onefinestay.com>
2013-04-18 11:27:28 +0100

    Get storage from pytest fixture

--- test/test_unique.py
+++ test/test_unique.py
@@ -9,7 +9,6 @@ from orp.persistence import Storage
 from orp.types import PersistableType, Persistable, Relationship
 from orp.attributes import Integer, String
 
-conn_uri = 'http://localhost:7474/db/data'
 
 
 class NoIndexThing(Persistable):
@@ -28,29 +27,24 @@ class Follows(Relationship):
 
 class TestReplace(object):
 
-    def setup_method(self, method):
-        conn = get_connection(conn_uri)
-        conn.clear()
-        self.store = Storage(conn_uri)
-
-    def test_unique_enforced_on_add(self):
+    def test_unique_enforced_on_add(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='lunch')
-        self.store.add(obj1)
+        storage.add(obj1)
 
         # should add as no unique clash
         obj2 = UniqueThing(id=1, code='B', extra='snacks')
         with pytest.raises(UniqueConstraintError):
-            self.store.add(obj2)
+            storage.add(obj2)
 
-    def test_replace_no_conflict(self):
+    def test_replace_no_conflict(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='lunch')
-        self.store.replace(obj1)
+        storage.replace(obj1)
 
         # should add as no unique clash
         obj2 = UniqueThing(id=2, code='B', extra='snacks')
-        self.store.replace(obj2)
+        storage.replace(obj2)
 
-        rows = self.store.query('''START n = node:uniquething("id:*")
+        rows = storage.query('''START n = node:uniquething("id:*")
                                    RETURN n''')
         rows = list(rows)
 
@@ -59,61 +53,61 @@ class TestReplace(object):
         assert rows[0][0].id == 1
         assert rows[1][0].id == 2
 
-    def test_no_change(self):
+    def test_no_change(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='lunch')
-        self.store.replace(obj1)
+        storage.replace(obj1)
 
         # should be a no-op, end up with only one obj in db
         obj2 = UniqueThing(id=1, code='A', extra='lunch')
-        self.store.replace(obj2)
+        storage.replace(obj2)
 
-        rows = self.store.query('''START n = node:uniquething("id:*")
+        rows = storage.query('''START n = node:uniquething("id:*")
                                    RETURN n''')
         rows = list(rows)
         assert len(rows) == 1
 
-    def test_unique_fail(self):
+    def test_unique_fail(self, storage):
         obj1 = UniqueThing(id=1, code='A')
-        self.store.replace(obj1)
+        storage.replace(obj1)
 
         obj2 = UniqueThing(id=2, code='C')
-        self.store.replace(obj2)
+        storage.replace(obj2)
 
         # no way to add this thing without breaking a unique constraint
         with pytest.raises(UniqueConstraintError):
             obj3 = UniqueThing(id=1, code='C')
-            self.store.replace(obj3)
+            storage.replace(obj3)
 
-    def test_change_non_unique_field(self):
+    def test_change_non_unique_field(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='chocolate')
-        self.store.replace(obj1)
+        storage.replace(obj1)
 
         obj2 = UniqueThing(id=1, code='A', extra='ice cream')
-        self.store.replace(obj2)
-        rows = self.store.query('''START n = node:uniquething("id:*")
+        storage.replace(obj2)
+        rows = storage.query('''START n = node:uniquething("id:*")
                                    RETURN n''')
         rows = list(rows)
         assert len(rows) == 1
         assert rows[0][0].extra == 'ice cream'
 
-    def test_remove_prop(self):
+    def test_remove_prop(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='chocolate')
-        self.store.replace(obj1)
+        storage.replace(obj1)
 
         obj2 = UniqueThing(id=1, code='A')
-        self.store.replace(obj2)
-        rows = self.store.query('''START n = node:uniquething("id:*")
+        storage.replace(obj2)
+        rows = storage.query('''START n = node:uniquething("id:*")
                                    RETURN n''')
         rows= list(rows)
         assert len(rows) == 1
         assert rows[0][0].extra is None
 
-    def test_cant_replace_non_indexed(self):
+    def test_cant_replace_non_indexed(self, storage):
         obj1 = NoIndexThing(field='a')
         with pytest.raises(NoIndexesError):
-            self.store.replace(obj1)
+            storage.replace(obj1)
 
-    def test_no_existing_index(self):
+    def test_no_existing_index(self, storage):
         name = ''.join(random.choice(string.ascii_letters) for i in range(20))
 
         # we have no way of removing indexes from the db, so create a new
@@ -123,21 +117,21 @@ class TestReplace(object):
             {'code': String(unique=True)})
 
         obj = RandomThing(code='a')
-        self.store.add(obj)
+        storage.add(obj)
 
-    def test_rel_uniqueness(self):
+    def test_rel_uniqueness(self, storage):
         obj1 = UniqueThing(id=1, code='A', extra='lunch')
         obj2 = UniqueThing(id=2, code='B', extra='snacks')
-        self.store.replace(obj1)
-        self.store.replace(obj2)
+        storage.replace(obj1)
+        storage.replace(obj2)
 
         follow_rel1 = Follows(obj1, obj2)
-        self.store.replace(follow_rel1)
+        storage.replace(follow_rel1)
 
         follow_rel2 = Follows(obj1, obj2)
-        self.store.replace(follow_rel2)
+        storage.replace(follow_rel2)
 
-        result = self.store.query('''
+        result = storage.query('''
             START n = node:uniquething(id="1")
             MATCH n-[r:FOLLOWS]->()
             RETURN r''')
