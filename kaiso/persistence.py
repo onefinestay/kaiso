7e07c7af1278a81523fa88f89f8e8672785b0afc
David Szotten <davidszotten@gmail.com>
2013-05-03 14:16:24 +0100

    don't break if endpoints are missing
    e.g. after serialization

--- kaiso/persistence.py
+++ kaiso/persistence.py
@@ -203,14 +203,16 @@ class Storage(object):
                 if isinstance(persistable, Relationship):
                     ex_start = get_attr_filter(existing.start)
                     ex_end = get_attr_filter(existing.end)
-                    pers_start = get_attr_filter(persistable.start)
-                    pers_end = get_attr_filter(persistable.end)
 
-                    if ex_start != pers_start:
-                        props['start'] = persistable.start
+                    if hasattr(persistable, 'start'):
+                        pers_start = get_attr_filter(persistable.start)
+                        if ex_start != pers_start:
+                            props['start'] = persistable.start
 
-                    if ex_end != pers_end:
-                        props['end'] = persistable.end
+                    if hasattr(persistable, 'end'):
+                        pers_end = get_attr_filter(persistable.end)
+                        if ex_end != pers_end:
+                            props['end'] = persistable.end
 
                 if existing_props == props:
                     return existing, {}
