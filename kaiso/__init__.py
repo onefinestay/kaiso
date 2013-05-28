1d2bf3ab2d79ec16858d90cb2976a5e3f2928121
David Szotten <davidszotten@gmail.com>
2013-05-28 11:48:12 +0100

    add version attr

--- kaiso/__init__.py
+++ kaiso/__init__.py
@@ -3,3 +3,9 @@ for objects and their relationships.
 
 TODO: describe the architecture
 """
+
+
+try:
+    VERSION = __import__('pkg_resources').get_distribution('kaiso').version
+except:
+    VERSION = 'unknown'
