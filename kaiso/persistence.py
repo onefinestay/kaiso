c88e95003b58eeb6065f3728ef07320699a3eade
Jan-Klaas Kollhof <jan@kollhof.net>
2013-08-05 14:17:31 +0100

    fixing type inheritence

--- kaiso/persistence.py
+++ kaiso/persistence.py
@@ -13,7 +13,7 @@ from kaiso.relationships import InstanceOf
 from kaiso.serialize import dict_to_db_values_dict, get_changes
 from kaiso.types import (
     Descriptor, Persistable, PersistableType, Relationship, TypeRegistry,
-    AttributedBase, get_index_name, get_type_id, is_indexable)
+    AttributedBase, Entity, get_index_name, get_type_id, is_indexable)
 
 
 log = getLogger(__name__)
@@ -176,9 +176,17 @@ class Manager(object):
                 cls = None
 
             if cls is None:
-                bases = tuple(registry.get_class_by_id(base) for base in bases)
+                base_classes = []
+                for base in bases:
+                    if base == 'Entity':
+                        # For the root of types we need to use the real
+                        base_cls = Entity
+                    else:
+                        base_cls = registry.get_descriptor_by_id(base).cls
+                    base_classes.append(base_cls)
+
                 attrs = dict((attr.name, attr) for attr in attrs)
-                registry.create_type(str(type_id), bases, attrs)
+                registry.create_type(str(type_id), tuple(base_classes), attrs)
 
         Manager._type_registry_cache = (
             self.type_registry.clone(),
