622a9612f3537d6e74e45d832c40fad623ed31b8
David Szotten <davidszotten@gmail.com>
2013-12-18 16:52:39 +0000

    bump version + CHANGES

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ test_requirements, dependency_links = parse_requirements(
 
 setup(
     name='kaiso',
-    version='0.13.4',
+    version='0.14.0',
     description='A queryable object persistence and relationship framework'
                 ' based on the Neo4j graph database.',
     author='onefinestay',
